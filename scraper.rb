02970c69061cc3b3a16830bf39ec3e2d07910262
Luke Bacon <luke@equivalentideas.com>
2016-02-19 07:23:24 +1100

    Update scraper for new website
    The website this needs to scrape has changed

--- scraper.rb
+++ scraper.rb
@@ -14,24 +14,35 @@ end
 def scrape_council(url)
   agent = Mechanize.new
   page = agent.get(url)
+
   council = page.at("h1").inner_text
-  website_h = page.search("h2").find{|h| h.inner_text == "Website"}
-  website = website_h.next_element.inner_text
-  h = page.search("h2").find{|h| h.inner_text == "Councillors"}
-  block = h.next_element.inner_html.split("<br>")
-  block[1..-1].each do |line|
-    if line.split(" - ")[0].strip == "Unsubdivided"
-      ward = nil
+
+  contact_keys = page.at(".council-profile-contact-information")
+                     .search(".council-profile-contact-title")
+
+  website_key = contact_keys.find{ |elm| elm.text == "Web:" }
+  website = website_key.next_element.children[0][:href]
+
+  councillor_list_elements = page.at(".councillors")
+                                 .children
+                                 .select { |elm| elm.text.include?(" Cr ") }
+  # Do a check against the number of councillors described
+
+  councillor_list_elements.each do |element|
+    text = element.text
+
+    ward = if text.include? "Unsubdivided"
+      nil
     else
-      ward = line.split(" - ")[0].strip
+      text.split(" - ")[0].strip
     end
 
-    if line.split(" - ")[1..-1].join(" - ").strip =~ /<strong>\(Mayor.*\)<\/strong>/
-      name = simplify_name(line.split(" - ")[1..-1].join(" - ").strip.split("<strong>").first.strip)
+    if text.include?("(Mayor")
       position = "mayor"
+      name = text.sub(/^.*Cr/, "").strip.sub(/[(].*$/, "").strip
     else
-      name = simplify_name(line.split(" - ")[1..-1].join(" - ").strip)
       position = nil
+      name = text.sub(/^.*Cr/, "").strip
     end
 
     record = {
@@ -41,21 +52,17 @@ def scrape_council(url)
       "position" => position,
       "council_website" => website
     }
+
     p record
+
     ScraperWiki.save_sqlite(["council", "councillor"], record)
   end
 end
 
 agent = Mechanize.new
 
-page = agent.get("http://www.dtpli.vic.gov.au/local-government/find-your-local-council")
+page = agent.get("https://knowyourcouncil.vic.gov.au/councils")
 
-urls = page.at("#councils").parent.search("a").map {|a| a["href"]}
-# Ignore the last link because it's for contact details
-if urls[-1] == "http://www.dtpli.vic.gov.au/local-government/local-government-contact-details"
-  urls = urls[0..-2]
-else
-  raise "Unexpected form of last link"
-end
+urls = page.at("#myc-council-list").search("a").map {|a| a["href"]}
 
 urls.each {|url| scrape_council(url)}
